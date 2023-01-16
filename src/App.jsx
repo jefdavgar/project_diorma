0434a3808fdf79c256c6d36585c80b11f4991590
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-01-16 10:13:49 -0500

    First components update. Change Button style and component configurate with name Link.jsx. Finally, we finished the Link style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Button from "./components/atom/Button/Button"
+import Link from "./components/atom/Link/Link"
 function App() {
   return (
     <div>
         <Button text="Presioname" />
+        <Link text="Hola soy un link"></Link>
     </div>
   )
 }
