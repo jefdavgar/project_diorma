9c23831cab1a3911ecb7907bfa02c0b18c2449bf
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-01-17 12:23:47 -0500

    React bootstrap was installed, likewise the menu.jsx component was implemented with the complete navigation structure, only the logo is missing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Button from "./components/atom/Button/Button"
 import Menu from './components/molecules/Menu/Menu'
+import Link from './components/atom/Link/Link'
 import ButtonProduct from './components/atom/ButtonProduct/ButtonProduct'
 import ButtonCategory from './components/atom/ButtonCategory/ButtonCategory'
 import QuantityButton from './components/atom/QuantityButton/QuantityButton'
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
         <Button text="Presioname" />
+        <Link text="Home"></Link>
         <Menu></Menu>
         <ButtonProduct text="ver"></ButtonProduct>
         <ButtonCategory text="Hola"></ButtonCategory>
--- src/components/molecules/Menu/Menu.jsx
+++ src/components/molecules/Menu/Menu.jsx
@@ -1,14 +1,27 @@
-import NavLink from '../../atom/Link/NavLink'
-import { BrowserRouter as Router } from 'react-router-dom'
-
-const Menu = () => {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Link from '../../atom/Link/Link';
+import "./css/Menu.css"
+function Menu() {
   return (
-    <Router>
-        <nav>
-          <NavLink text="Menu"></NavLink>
-        </nav>
-    </Router>
-  )
+    <Navbar className='Navbar' expand="lg" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle className='custom-button' aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+          </Nav>
+          <Nav>
+            <Nav.Link href="#home"><Link text="DESCUBRE"></Link></Nav.Link>
+            <Nav.Link href="#home"><Link text="HISTORIA"></Link></Nav.Link>
+            <Nav.Link href="#home"><Link text="PRODUCTOS"></Link></Nav.Link>
+            <Nav.Link href="#home"><Link text="COLECCIÓN"></Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Menu
+export default Menu;
