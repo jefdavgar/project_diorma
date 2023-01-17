bf2ebfd59b7ac4945467ddd5b4ac8875022af6c4
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-01-17 09:09:12 -0500

    The components of the molecules folder are created and given structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Button from "./components/atom/Button/Button"
-import Link from "./components/atom/Link/Link"
+import Menu from './components/molecules/Menu/Menu'
 import ButtonProduct from './components/atom/ButtonProduct/ButtonProduct'
 import ButtonCategory from './components/atom/ButtonCategory/ButtonCategory'
 import QuantityButton from './components/atom/QuantityButton/QuantityButton'
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
         <Button text="Presioname" />
-        <Link text="Hola soy un link"></Link>
+        <Menu></Menu>
         <ButtonProduct text="ver"></ButtonProduct>
         <ButtonCategory text="Hola"></ButtonCategory>
         <QuantityButton></QuantityButton>
---
+++ src/components/molecules/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavLink from '../../atom/Link/Link'
+const Menu = () => {
+  return (
+    <div>
+        <NavLink text="MENU"></NavLink>
+    </div>
+  )
+}
+
+export default Menu
