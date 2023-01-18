987a1893da91bbe7f9ecd31da077b15c9a92303e
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-01-18 15:57:29 -0500

    First preview of the content of the collection section, it was moved from the ProductCard folder to the organisms folder

--- src/components/pages/Products/Products.jsx
+++ src/components/pages/Products/Products.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Footer from '../../organisms/Footer/Footer'
 import Header from '../../organisms/Header/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Container, Image } from 'react-bootstrap';
+import ButtonCategory from '../../atom/ButtonCategory/ButtonCategory';
+import bannerbag from "../../../assets/img/banner/bannerbags.jpg"
+import "./css/Products.css"
 const Products = () => {
   return (
     <>
         <Header></Header>
-        
+        <Container>
+           <ButtonCategory text="Todo"></ButtonCategory>
+           <ButtonCategory text="Bolsos"></ButtonCategory>
+           <ButtonCategory text="Tops"></ButtonCategory>
+        </Container>
         <Footer></Footer>
     </>
   )
