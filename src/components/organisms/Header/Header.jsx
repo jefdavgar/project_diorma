ffa0e44ee000e6feec4520f8fea532c37dcd301c
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-01-18 13:12:14 -0500

    The navigation menu for the products section was configured, the title of the section and the shopping cart are missing

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -4,24 +4,23 @@ import { Container, Nav, Navbar } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom';
 import Link from '../../atom/Link/Link';
 import logo from "../../../assets/img/logo/Logo_responsive.png"
+import "./css/Header.css"
+import img_arrow from "../../../assets/img/icons/back arrow.png"
 const Header = () => {
   return (
-    <Navbar className='Navbar' expand="lg" fixed="top">
+    <Navbar className='custom-Navbar' expand="lg" fixed="top">
       <Container>
-        <NavLink to="/home"><img src={logo} alt="logo" /></NavLink>
-        <Navbar.Toggle className='custom-button' aria-controls="basic-navbar-nav" />
+        <NavLink to="/home" className="img_arrow"><img src={img_arrow} alt="logo" /></NavLink>
+        <NavLink to="/home" className="logo"><img src={logo} alt="logo" /></NavLink>
+        <Navbar.Toggle className='custom-button custom_two' aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
           </Nav>
           <Nav>
-            <NavLink to="/home"><Link text="DESCUBRE"></Link></NavLink>
-            <br />
-            <NavLink to="/home"><Link text="HISTORIA"></Link></NavLink>
-            <br />
-            <NavLink to="/home"><Link text="PRODUCTOS"></Link></NavLink>
-            <br />
-            <NavLink to="/home"><Link text="COLECCIÓN"></Link></NavLink>
-            <br />
+            <NavLink to="/home" className="Nav-Link"><Link text="DESCUBRE"></Link></NavLink>
+            <NavLink to="/home" className="Nav-Link"><Link text="HISTORIA"></Link></NavLink>
+            <NavLink to="/home" className="Nav-Link"><Link text="PRODUCTOS"></Link></NavLink>
+            <NavLink to="/home" className="Nav-Link"><Link text="COLECCIÓN"></Link></NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
